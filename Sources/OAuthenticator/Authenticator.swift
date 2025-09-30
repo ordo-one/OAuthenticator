c6cbe408d8ce4a3241aa45efe23d0ac65d004591
Tim <0xtimc@gmail.com>
2025-09-30 11:50:54 +0100

    Remove Sendable requirements

--- Sources/OAuthenticator/Authenticator.swift
+++ Sources/OAuthenticator/Authenticator.swift
@@ -30,7 +30,7 @@ public enum AuthenticatorError: Error, Hashable {
 /// Manage state required to executed authenticated URLRequests.
 public actor Authenticator {
 	public typealias UserAuthenticator = @Sendable (URL, String) async throws -> URL
-    public typealias AuthenticationStatusHandler = @Sendable (Result<Login, AuthenticatorError>) -> Void
+    public typealias AuthenticationStatusHandler = (Result<Login, AuthenticatorError>) -> Void
     
 	/// A `UserAuthenticator` that always fails. Useful as a placeholder
 	/// for testing and for doing manual authentication with an external
@@ -64,7 +64,7 @@ public actor Authenticator {
 		}
 	}
 
-	public struct Configuration: Sendable {
+	public struct Configuration {
 		public let appCredentials: AppCredentials
 
 		public let loginStorage: LoginStorage?
